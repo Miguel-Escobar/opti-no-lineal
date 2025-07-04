2a1e9b35709500f834ff6624d9628929aca0e13f
Miguel <77211681+Miguel-Escobar@users.noreply.github.com>
2025-07-04 15:52:43 -0400

    better way

--- control-3/src/main.rs
+++ control-3/src/main.rs
@@ -14,35 +14,27 @@ fn fn_for_sgd<const N: usize>(x: &[f64; N]) -> f64 {
         .sum()
 }
 
-fn batch_gradf<const N: usize>(x: &[f64; N], index: usize) -> [f64; N] {
-    std::array::from_fn(|i| {
-        if i == index {
-            match i {
-                i if (1..(N - 1)).contains(&i) => {
-                    -200.0 * (x[i + 1] - x[i].powi(2)) * x[i] - 2.0 * (1.0 - x[i])
-                        + 100.0 * (x[i] - x[i - 1].powi(2))
-                }
-                i if i == N - 1 => 100.0 * (x[i] - x[i - 1].powi(2)),
-                i if i == 0 => -200.0 * (x[i + 1] - x[i].powi(2)) * x[i] - 2.0 * (1.0 - x[i]),
-                _ => 0.0,
-            }
-        } else {
-            0.0
+fn element_wise_gradf<const N: usize>(x: &[f64; N], index: usize) -> f64 {
+    match index {
+        index if index == N - 1 => 100.0 * (x[index] - x[index - 1].powi(2)),
+        index if index == 0 => {
+            -200.0 * (x[1] - x[index].powi(2)) * x[index] - 2.0 * (1.0 - x[index])
         }
-    })
+        _ => {
+            -200.0 * (x[index + 1] - x[index].powi(2)) * x[index] - 2.0 * (1.0 - x[index])
+                + 100.0 * (x[index] - x[index - 1].powi(2))
+        }
+    }
 }
 
-fn nesterov(
-    _f: fn(&[f64; 2]) -> f64,
-    gradf: fn(&[f64; 2]) -> [f64; 2],
-    x0: [f64; 2],
-    alpha: f64,
-    beta: f64,
-    n: u32,
-) -> [f64; 2] {
+fn nesterov<F, G>(_f: F, gradf: G, x0: [f64; 2], alpha: f64, beta: f64, n: u32) -> [f64; 2]
+where
+    F: Fn(&[f64; 2]) -> f64,
+    G: Fn(&[f64; 2]) -> [f64; 2],
+{
     let mut x_present: [f64; 2] = x0;
     let mut x_past: [f64; 2] = x0;
-    for _ in 1..n {
+    for _ in 0..n {
         let v: [f64; 2] = std::array::from_fn(|i| x_present[i] + beta * (x_present[i] - x_past[i]));
         let grad_v: [f64; 2] = gradf(&v);
         x_past = x_present;
@@ -51,28 +43,37 @@ fn nesterov(
     x_present
 }
 
-fn sgd<const N: usize>(
-    _f: fn(&[f64; N]) -> f64,
-    gradf: fn(&[f64; N], usize) -> [f64; N],
-    x0: [f64; N],
-    alpha: f64,
-    n: u32,
-) -> [f64; N] {
+fn sgd<const N: usize, F, G>(_f: F, gradf: G, x0: [f64; N], alpha: f64, n: u32) -> [f64; N]
+where
+    F: Fn(&[f64; N]) -> f64,
+    G: Fn(&[f64; N], usize) -> f64,
+{
     let mut x_present: [f64; N] = x0.clone();
     let mut rng = rand::rng();
     for _ in 1..n {
-        let grad = gradf(&x_present, rng.random_range(0..N));
-        x_present = std::array::from_fn(|i| x_present[i] - alpha * grad[i]);
+        let index = rng.random_range(0..N);
+        let grad = gradf(&x_present, index);
+        x_present[index] -= alpha * grad;
     }
     x_present
 }
 
 fn main() {
     let point_sgd: [f64; 1000] = [0.5; 1000];
-    let test_val_sgd: [f64; 1000] = sgd(fn_for_sgd, batch_gradf, point_sgd, 0.001, 1_000_000);
+    let test_val_sgd: [f64; 1000] =
+        sgd(fn_for_sgd, element_wise_gradf, point_sgd, 0.001, 1_000_000);
     println!("final val sgd = {}", fn_for_sgd(&test_val_sgd));
-
     let point_nesterov: [f64; 2] = [3.0, 5.0];
-    let test_val_nesterov: [f64; 2] = nesterov(fn_for_nesterov, gradf_for_nesterov, point_nesterov, 0.1, 0.1, 100);
-    println!("final val nesterov = {}", fn_for_nesterov(&test_val_nesterov))
+    let test_val_nesterov: [f64; 2] = nesterov(
+        fn_for_nesterov,
+        gradf_for_nesterov,
+        point_nesterov,
+        0.1,
+        0.1,
+        100,
+    );
+    println!(
+        "final val nesterov = {}",
+        fn_for_nesterov(&test_val_nesterov)
+    )
 }
